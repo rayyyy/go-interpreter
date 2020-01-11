3a0e1063fe87421c13638d57a6adc5107dc931a9
rayyyy <nraavy@gmail.com>
2020-01-12 01:30:11 +0900

    lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,64 @@
+package lexer
+
+import (
+	"github.com/rayyyy/go-interpreter/token"
+)
+
+// Lexer 字句解析器
+type Lexer struct {
+	input        string
+	position     int  // 入力の現在の位置
+	readPosition int  // これから読み込む位置（現在の位置の次）
+	ch           byte // 現在検査中の文字
+}
+
+// New 字句解析器を作成
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+// NextToken 文字からトークンを返す
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.Type, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
